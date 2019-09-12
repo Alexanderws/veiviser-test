1961696637c28a7db70cb36f215ecfcef23cfb33
alexanderws <Alex.ws@icloud.com>
2019-09-12 21:45:05 +0200

    Updates

---
+++ precache-manifest.9fa00325cab874d163072228b01d1b23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a5abb34ef28619edfd86b0e66ea8fe1",
+    "url": "/veiviser-test/index.html"
+  },
+  {
+    "revision": "6e2fcb0301d78254ac59",
+    "url": "/veiviser-test/static/css/main.d1eb9ee9.chunk.css"
+  },
+  {
+    "revision": "93a55d70c8c89a62d88d",
+    "url": "/veiviser-test/static/js/2.a09bc966.chunk.js"
+  },
+  {
+    "revision": "6e2fcb0301d78254ac59",
+    "url": "/veiviser-test/static/js/main.2de6192e.chunk.js"
+  },
+  {
+    "revision": "766004cd10ebfe6d53cb",
+    "url": "/veiviser-test/static/js/runtime~main.f5bc01ef.js"
+  }
+]);
